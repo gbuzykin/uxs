fdb0d09b93b0b3908db1267281f594547c7fe187
Georgii Buzykin <gbuzykin@yandex.ru>
2022-03-19 17:26:13 +0300

    utf: Refactor converters

--- include/util/utf.h
+++ include/util/utf.h
@@ -7,36 +7,40 @@
 namespace util {
 
 template<typename InputIt>
-InputIt from_utf8(InputIt first, InputIt last, uint32_t* pcode) {
-    if (first == last) { return first; }
-    uint32_t code = static_cast<uint8_t>(*first);
+unsigned from_utf8(InputIt first, InputIt last, InputIt& next, uint32_t& code) {
+    next = first;
+    if (first == last) { return 0; }
+    code = static_cast<uint8_t>(*first++);
+    unsigned n_read = 1;
     if (code >= 0xc0 && code < 0xf8) {
         unsigned count = code < 0xe0 ? 1 : (code < 0xf0 ? 2 : 3);
-        if (last - first <= count) { return first; }
-        const uint8_t mask[] = {0x1f, 0x1f, 0xf, 0x7};
-        code &= mask[count];
-        do { code = (code << 6) | ((*++first) & 0x3f), --count; } while (count > 0);
+        const uint8_t mask[] = {0, 0x1f, 0xf, 0x7};
+        code &= mask[count], n_read += count;
+        do {
+            if (first == last) { return 0; }
+            code = (code << 6) | (*first++ & 0x3f);
+        } while (--count > 0);
     }
-    *pcode = code;
-    return ++first;
+    next = first;
+    return n_read;
 }
 
 template<typename InputIt>
-InputIt from_utf16(InputIt first, InputIt last, uint32_t* pcode) {
-    if (first == last) { return first; }
-    uint32_t code = static_cast<uint16_t>(*first);
+unsigned from_utf16(InputIt first, InputIt last, InputIt& next, uint32_t& code) {
+    next = first;
+    if (first == last) { return 0; }
+    code = static_cast<uint16_t>(*first++);
+    unsigned n_read = 1;
     if ((code & 0xdc00) == 0xd800) {
-        if (last - first <= 1) { return first; }
-        code = 0x10000 + ((code & 0x3ff) << 10) | ((*++first) & 0x3ff);
+        if (first == last) { return 0; }
+        code = 0x10000 + ((code & 0x3ff) << 10) | (*first++ & 0x3ff), ++n_read;
     }
-    *pcode = code;
-    return ++first;
+    next = first;
+    return n_read;
 }
 
 template<typename OutputIt>
-unsigned to_utf8(uint32_t code, OutputIt out,  //
-                 size_t max_count = std::numeric_limits<size_t>::max()) {
-    if (!max_count) { return 0; }
+unsigned to_utf8(uint32_t code, OutputIt out) {
     if (code < 0x80) {
         *out++ = static_cast<uint8_t>(code);
         return 1;
@@ -46,24 +50,56 @@ unsigned to_utf8(uint32_t code, OutputIt out,  //
     const uint8_t hdr[] = {0, 0xc0, 0xe0, 0xf0};
     unsigned count = 0;
     if (code >= 0x110000) { code = 0xfffd; }
-    do {
-        ch[count++] = 0x80 | (code & 0x3f);
-        code >>= 6;
-    } while (code > mask[count]);
-    if (count >= max_count) { return 0; }
+    do { ch[count] = 0x80 | (code & 0x3f); } while ((code >>= 6) > mask[++count]);
     *out++ = static_cast<uint8_t>(hdr[count] | code);
-    const unsigned n = count + 1;
+    const unsigned n_written = count + 1;
     do { *out++ = ch[--count]; } while (count > 0);
-    return n;
+    return n_written;
 }
 
 template<typename OutputIt>
-unsigned to_utf16(uint32_t code, OutputIt out,  //
-                  size_t max_count = std::numeric_limits<size_t>::max()) {
-    if (!max_count) { return 0; }
+unsigned to_utf8_n(uint32_t code, OutputIt out, size_t n) {
+    if (n == 0) { return 0; }
+    if (code < 0x80) {
+        *out++ = static_cast<uint8_t>(code);
+        return 1;
+    }
+    uint8_t ch[4];
+    const uint8_t mask[] = {0, 0x1f, 0xf, 0x7};
+    const uint8_t hdr[] = {0, 0xc0, 0xe0, 0xf0};
+    unsigned count = 0;
+    if (code >= 0x110000) { code = 0xfffd; }
+    do { ch[count] = 0x80 | (code & 0x3f); } while ((code >>= 6) > mask[++count]);
+    if (n <= count) { return 0; }
+    *out++ = static_cast<uint8_t>(hdr[count] | code);
+    const unsigned n_written = count + 1;
+    do { *out++ = ch[--count]; } while (count > 0);
+    return n_written;
+}
+
+template<typename OutputIt>
+unsigned to_utf16(uint32_t code, OutputIt out) {
     if (code >= 0x10000) {
         if (code < 0x110000) {
-            if (max_count <= 1) { return 0; }
+            code -= 0x10000;
+            *out++ = static_cast<uint16_t>(0xd800 | (code >> 10));
+            *out++ = static_cast<uint16_t>(0xdc00 | (code & 0x3ff));
+            return 2;
+        }
+        code = 0xfffd;
+    } else if ((code & 0xd800) == 0xd800) {
+        code = 0xfffd;
+    }
+    *out++ = static_cast<uint16_t>(code);
+    return 1;
+}
+
+template<typename OutputIt>
+unsigned to_utf16_n(uint32_t code, OutputIt out, size_t n) {
+    if (n == 0) { return 0; }
+    if (code >= 0x10000) {
+        if (code < 0x110000) {
+            if (n <= 1) { return 0; }
             code -= 0x10000;
             *out++ = static_cast<uint16_t>(0xd800 | (code >> 10));
             *out++ = static_cast<uint16_t>(0xdc00 | (code & 0x3ff));
--- src/stringalg.cpp
+++ src/stringalg.cpp
@@ -8,9 +8,11 @@ std::wstring util::from_utf8_to_wide(std::string_view s) {
     uint32_t code;
     std::wstring result;
     result.reserve(s.size());
-    for (auto p = s.begin(), p1 = p; (p1 = from_utf8(p, s.end(), &code)) > p; p = p1) {
-        to_utf16(code, std::back_inserter(result));
-    }
+#if defined(WCHAR_MAX) && WCHAR_MAX > 0xffff
+    for (auto p = s.begin(); from_utf8(p, s.end(), p, code) != 0;) { result.push_back(static_cast<wchar_t>(code)); }
+#else   // define(WCHAR_MAX) && WCHAR_MAX > 0xffff
+    for (auto p = s.begin(); from_utf8(p, s.end(), p, code) != 0;) { to_utf16(code, std::back_inserter(result)); }
+#endif  // define(WCHAR_MAX) && WCHAR_MAX > 0xffff
     return result;
 }
 
@@ -18,9 +20,11 @@ std::string util::from_wide_to_utf8(std::wstring_view s) {
     uint32_t code;
     std::string result;
     result.reserve(s.size());
-    for (auto p = s.begin(), p1 = p; (p1 = from_utf16(p, s.end(), &code)) > p; p = p1) {
-        to_utf8(code, std::back_inserter(result));
-    }
+#if defined(WCHAR_MAX) && WCHAR_MAX > 0xffff
+    for (auto p = s.begin(); p != s.end(); ++p) { to_utf8(static_cast<uint32_t>(*p), std::back_inserter(result)); }
+#else   // define(WCHAR_MAX) && WCHAR_MAX > 0xffff
+    for (auto p = s.begin(); from_utf16(p, s.end(), p, code) != 0;) { to_utf8(code, std::back_inserter(result)); }
+#endif  // define(WCHAR_MAX) && WCHAR_MAX > 0xffff
     return result;
 }
 
