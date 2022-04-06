9430116e2072f057aed4829a2452b608a11ddd02
Georgii Buzykin <gbuzykin@yandex.ru>
2022-04-06 14:50:02 +0300

    Introduce IO buffers for strings

---
+++ src/io/istringbuf.cpp
@@ -0,0 +1,6 @@
+#include "util/io/istringbuf_impl.h"
+
+namespace util {
+template class basic_istringbuf<char>;
+template class basic_istringbuf<wchar_t>;
+}  // namespace util
---
+++ src/io/ostringbuf.cpp
@@ -0,0 +1,6 @@
+#include "util/io/ostringbuf_impl.h"
+
+namespace util {
+template class basic_ostringbuf<char>;
+template class basic_ostringbuf<wchar_t>;
+}  // namespace util
