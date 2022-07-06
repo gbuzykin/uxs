19e57fa6f518e65ba99ed597f21f14165e6b51ed
Georgii Buzykin <gbuzykin@yandex.ru>
2022-07-06 21:51:43 +0300

    pool_allocator: Customize allocator

--- src/pool_allocator.cpp
+++ src/pool_allocator.cpp
@@ -1,40 +1,8 @@
-#include "uxs/pool_allocator.h"
+#include "uxs/pool_allocator_impl.h"
 
-using namespace uxs;
-
-/*static*/ pool pool::global_pool_;
-
-/*static*/ void pool::tidy(pool_desc_t* desc) {
-    auto* desc0 = desc;
-    do {
-        auto* next = desc->next_pool;
-        if (desc->size_and_alignment) { desc->tidy_pool(desc); }
-        alloc_type().deallocate(desc, 1);
-        desc = next;
-    } while (desc != desc0);
-}
-
-/*static*/ pool::pool_desc_t* pool::find_pool(pool_desc_t* desc, uint32_t size_and_alignment) {
-    auto* desc0 = desc;
-    do {
-        if (desc->size_and_alignment == size_and_alignment) { return desc; }
-        desc = desc->next_pool;
-    } while (desc != desc0);
-    return nullptr;
-}
-
-/*static*/ pool::pool_desc_t* pool::allocate_new_pool() {
-    auto* desc = alloc_type().allocate(1);
-    dllist_make_cycle(&desc->free);
-    dllist_make_cycle(&desc->partitions);
-    return desc;
-}
-
-/*static*/ pool::pool_desc_t* pool::allocate_dummy_pool(uint32_t partition_size) {
-    auto* desc = allocate_new_pool();
-    desc->next_pool = desc->root_pool = desc;
-    desc->size_and_alignment = 0;
-    desc->ref_count = 1;
-    desc->partition_size = partition_size;
-    return desc;
-}
+namespace uxs {
+namespace detail {
+pool<std::allocator<void>> g_global_pool;
+template class pool<std::allocator<void>>;
+}  // namespace detail
+}  // namespace uxs
