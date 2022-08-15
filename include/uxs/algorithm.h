5160a0da695421d9a0756550f2cca02cd9b4ede9
Georgii Buzykin <gbuzykin@yandex.ru>
2022-08-15 06:39:24 +0300

    algorithm: Add `for_loop` template function

--- include/uxs/algorithm.h
+++ include/uxs/algorithm.h
@@ -404,4 +404,37 @@ UnaryFunc for_each(Range&& r, UnaryFunc func) {
     return std::for_each(std::begin(r), std::end(r), func);
 }
 
+namespace detail {
+template<typename... Args>
+void dummy_func(Args&&...) {}
+template<typename Func, typename>
+struct for_loop_helper;
+template<typename Func>
+struct for_loop_helper<Func, std::false_type> {
+    template<typename Range, typename... InputIts>
+    auto operator()(Range&& r, Func func, InputIts... its) -> decltype(std::end(r)) {
+        auto it = std::begin(r), end_it = std::end(r);
+        for (; it != end_it; ++it, detail::dummy_func(++its...)) {
+            if (!func(*it, *its...)) { break; }
+        }
+        return it;
+    }
+};
+template<typename Func>
+struct for_loop_helper<Func, std::true_type> {
+    template<typename Range, typename... InputIts>
+    auto operator()(Range&& r, Func func, InputIts... its) -> decltype(std::end(r)) {
+        auto it = std::begin(r), end_it = std::end(r);
+        for (; it != end_it; ++it, detail::dummy_func(++its...)) { func(*it, *its...); }
+        return it;
+    }
+};
+}  // namespace detail
+
+template<typename Range, typename Func, typename... InputIts>
+auto for_loop(Range&& r, Func func, InputIts... its) -> decltype(std::end(r)) {
+    return detail::for_loop_helper<Func, typename std::is_same<decltype(func(*std::begin(r), *its...)), void>::type>()(
+        std::forward<Range>(r), func, its...);
+}
+
 }  // namespace uxs
