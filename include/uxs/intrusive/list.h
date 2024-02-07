c3c5c5084a9914d23eb685d8a027c21d8f81eaba
Georgii Buzykin <gbuzykin@yandex.ru>
2024-02-07 10:48:33 +0300

    intrusive::list: More improvements

--- include/uxs/intrusive/list.h
+++ include/uxs/intrusive/list.h
@@ -6,9 +6,11 @@
 namespace uxs {
 namespace intrusive {
 
-struct list_links_t : dllist_node_t {
+struct list_links_t {
+    list_links_t* next;
+    list_links_t* prev;
 #if _ITERATOR_DEBUG_LEVEL != 0
-    dllist_node_t* head;
+    list_links_t* head;
 #endif  // _ITERATOR_DEBUG_LEVEL != 0
 };
 
@@ -30,58 +32,99 @@ struct opt_member_hook;
 template<typename Ty, typename HookTy>
 struct list_hook_traits<Ty, HookTy> {
     using hook_t = HookTy;
-    using holding_pointer_t = std::remove_cv_t<Ty>*;
-    using is_release_modifying = std::false_type;
-    static Ty& get_value(hook_t* h) { return *static_cast<holding_pointer_t>(h); }
-    static void hold(hook_t* h, holding_pointer_t p) {}
-    static holding_pointer_t release(hook_t* h) { return static_cast<holding_pointer_t>(h); }
+    using owning_pointer_t = std::remove_cv_t<Ty>*;
+    Ty& get_value(hook_t* h) const { return *static_cast<owning_pointer_t>(h); }
+    owning_pointer_t release_pointer(hook_t* h) const { return static_cast<owning_pointer_t>(h); }
 };
 
 template<typename Ty, typename HookTy, typename Caster>
 struct list_hook_traits<Ty, HookTy, opt_hook_caster<Caster>> {
     using hook_t = HookTy;
-    using holding_pointer_t = decltype(Caster::cast(nullptr));
-    using is_release_modifying = std::false_type;
-    static Ty& get_value(hook_t* h) { return *Caster::cast(h); }
-    static void hold(hook_t* h, holding_pointer_t p) {}
-    static holding_pointer_t release(hook_t* h) { return Caster::cast(h); }
+    using owning_pointer_t = decltype(Caster{}.cast(nullptr));
+    Ty& get_value(hook_t* h) const { return *Caster{}.cast(h); }
+    owning_pointer_t release_pointer(hook_t* h) const { return Caster{}.cast(h); }
 };
 
 template<typename Ty, typename HookTy, typename InternalPtrTy, InternalPtrTy HookTy::*InternalPointerMember>
 struct list_hook_traits<Ty, HookTy, opt_internal_pointer<HookTy, InternalPtrTy, InternalPointerMember>> {
     using hook_t = HookTy;
-    using holding_pointer_t = InternalPtrTy;
-    static Ty& get_value(hook_t* h) { return *(h->*InternalPointerMember); }
-    static void hold(hook_t* h, holding_pointer_t p) { h->*InternalPointerMember = std::move(p); }
-    static holding_pointer_t release(hook_t* h) {
-        holding_pointer_t p = std::move(h->*InternalPointerMember);
-        h->*InternalPointerMember = nullptr;
-        return p;
-    }
+    using owning_pointer_t = InternalPtrTy;
+    Ty& get_value(hook_t* h) const { return *(h->*InternalPointerMember); }
+    owning_pointer_t release_pointer(hook_t* h) const { return std::move(h->*InternalPointerMember); }
+    void reset_pointer(hook_t* h, owning_pointer_t p) const { h->*InternalPointerMember = std::move(p); }
 };
 
 template<typename ParentTy, typename HookTy, HookTy ParentTy::*HookMember>
 struct list_hook_getter<opt_member_hook<ParentTy, HookTy, HookMember>> {
-    static HookTy* get_hook(ParentTy& parent) { return &(parent.*HookMember); }
+    HookTy* get_hook(ParentTy& parent) const { return &(parent.*HookMember); }
 };
 
 namespace detail {
 
 template<typename Ty, typename HookTraits>
 struct list_node_traits {
-    using iterator_node_t = dllist_node_t;
-    using links_t = list_links_t;
-    static dllist_node_t* get_next(dllist_node_t* node) { return node->next; }
-    static dllist_node_t* get_prev(dllist_node_t* node) { return node->prev; }
-    static Ty& get_value(dllist_node_t* node) {
-        return HookTraits::get_value(static_cast<typename HookTraits::hook_t*>(node));
+    using iterator_node_t = list_links_t;
+    using hook_t = typename HookTraits::hook_t;
+    using owning_pointer_t = typename HookTraits::owning_pointer_t;
+    static list_links_t* get_next(list_links_t* node) { return node->next; }
+    static list_links_t* get_prev(list_links_t* node) { return node->prev; }
+    static Ty& get_value(list_links_t* node) { return HookTraits{}.get_value(static_cast<hook_t*>(node)); }
+
+    static auto release_pointer(hook_t* h) -> decltype(HookTraits{}.release_pointer(h)) {
+        return HookTraits{}.release_pointer(h);
     }
+
+    template<typename HookTraits_ = HookTraits>
+    static auto reset_pointer(hook_t* h, owning_pointer_t p)
+        -> decltype(HookTraits_{}.reset_pointer(h, std::move(p)), int{}) {
+        HookTraits{}.reset_pointer(h, std::move(p));
+        return 0;
+    }
+    template<typename... Dummy>
+    static void reset_pointer(hook_t* h, owning_pointer_t p, Dummy&&...) {}
+
+    template<typename HookTraits_ = HookTraits>
+    static auto dispose(owning_pointer_t p) -> decltype(HookTraits_{}.dispose(std::move(p)), int{}) {
+        HookTraits{}.dispose(std::move(p));
+        return 0;
+    }
+    template<typename... Dummy>
+    static void dispose(Dummy&&...) {}
+
+    using has_reset_pointer = std::is_same<decltype(reset_pointer(nullptr, std::declval<owning_pointer_t>())), int>;
+    using has_dispose = std::is_same<decltype(dispose(std::declval<owning_pointer_t>())), int>;
+
+    template<typename Traits = list_node_traits,
+             typename = std::enable_if_t<Traits::has_reset_pointer::value || Traits::has_dispose::value>>
+    static void dispose_all(list_links_t* head) {
+        auto* item = head->next;
+        while (item != head) {
+            auto* next = item->next;
+            set_head(item, nullptr);
+            auto p = HookTraits{}.release_pointer(static_cast<hook_t*>(item));
+            reset_pointer(static_cast<hook_t*>(item), nullptr);
+            dispose(std::move(p));
+            item = next;
+        }
+    }
+    template<typename... Dummy>
+    static void dispose_all(list_links_t* head, Dummy&&...) {
 #if _ITERATOR_DEBUG_LEVEL != 0
-    static void set_head(dllist_node_t* node, dllist_node_t* head) { static_cast<links_t*>(node)->head = head; }
-    static dllist_node_t* get_head(dllist_node_t* node) { return static_cast<links_t*>(node)->head; }
-    static dllist_node_t* get_front(dllist_node_t* head) { return head->next; }
+        auto* item = head->next;
+        while (item != head) {
+            auto* next = item->next;
+            set_head(item, nullptr);
+            item = next;
+        }
+#endif  // _ITERATOR_DEBUG_LEVEL != 0
+    }
+
+#if _ITERATOR_DEBUG_LEVEL != 0
+    static void set_head(list_links_t* node, list_links_t* head) { node->head = head; }
+    static list_links_t* get_head(list_links_t* node) { return node->head; }
+    static list_links_t* get_front(list_links_t* head) { return head->next; }
 #else   // _ITERATOR_DEBUG_LEVEL != 0
-    static void set_head(dllist_node_t* node, dllist_node_t* head) {}
+    static void set_head(list_links_t* node, list_links_t* head) {}
 #endif  // _ITERATOR_DEBUG_LEVEL != 0
 };
 
@@ -108,7 +151,7 @@ class list_enumerator {
     using reverse_iterator = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
-    list_enumerator() {
+    list_enumerator() noexcept {
         dllist_make_cycle(&head_);
         node_traits::set_head(&head_, &head_);
     }
@@ -158,17 +201,16 @@ class list_enumerator {
     friend class list;
 
     size_t size_ = 0;
-    mutable typename node_traits::links_t head_;
+    mutable list_links_t head_;
 };
 
 template<typename Ty, typename HookTraits = list_hook_traits<Ty, list_links_t>, typename HookGetter = void>
 class list : public list_enumerator<Ty, HookTraits> {
  private:
-    using hook_traits = HookTraits;
     using hook_t = typename HookTraits::hook_t;
-    using holding_pointer_t = typename hook_traits::holding_pointer_t;
+    using owning_pointer_t = typename HookTraits::owning_pointer_t;
     using node_traits = typename list_enumerator<Ty, HookTraits>::node_traits;
-    using parent_t = std::remove_reference_t<decltype(*std::declval<holding_pointer_t>())>;
+    using parent_t = std::remove_reference_t<decltype(*std::declval<owning_pointer_t>())>;
 
  public:
     using reference = typename list_enumerator<Ty, HookTraits>::reference;
@@ -176,80 +218,63 @@ class list : public list_enumerator<Ty, HookTraits> {
     using iterator = typename list_enumerator<Ty, HookTraits>::iterator;
     using const_iterator = typename list_enumerator<Ty, HookTraits>::const_iterator;
 
-    ~list() { release_all(); }
+    ~list() { node_traits::dispose_all(&this->head_); }
 
-    void clear() {
-        release_all();
+    void clear() noexcept {
+        node_traits::dispose_all(&this->head_);
         this->size_ = 0;
         dllist_make_cycle(&this->head_);
     }
 
-    iterator insert(const_iterator pos, holding_pointer_t ptr) {
-        auto* item = get_hook(*ptr);
-        hook_traits::hold(item, std::move(ptr));
+    iterator insert(const_iterator pos, owning_pointer_t obj) {
+        auto* item = get_hook(*obj);
+        node_traits::reset_pointer(item, std::move(obj));
         node_traits::set_head(item, &this->head_);
         auto* after = pos.node();
         iterator_assert(node_traits::get_head(after) == &this->head_);
         ++this->size_;
-        dllist_insert_before<dllist_node_t>(after, item);
+        dllist_insert_before<list_links_t>(after, item);
         return iterator(item);
     }
 
-    reference push_front(holding_pointer_t ptr) { return *insert(this->begin(), std::move(ptr)); }
-    reference push_back(holding_pointer_t ptr) { return *insert(this->end(), std::move(ptr)); }
+    reference push_front(owning_pointer_t obj) { return *insert(this->begin(), std::move(obj)); }
+    reference push_back(owning_pointer_t obj) { return *insert(this->end(), std::move(obj)); }
 
-    std::pair<iterator, holding_pointer_t> extract(const_iterator pos) {
+    std::pair<iterator, owning_pointer_t> extract(const_iterator pos) {
         auto* item = pos.node();
         assert(item != &this->head_);
         iterator_assert(node_traits::get_head(item) == &this->head_);
         --this->size_;
         auto* next = dllist_remove(item);
         node_traits::set_head(item, nullptr);
-        return std::make_pair(iterator(next), hook_traits::release(static_cast<hook_t*>(item)));
+        auto obj = node_traits::release_pointer(static_cast<hook_t*>(item));
+        node_traits::reset_pointer(static_cast<hook_t*>(item), nullptr);
+        return std::make_pair(iterator(next), std::move(obj));
     }
 
-    holding_pointer_t extract_front() { return extract(this->begin()).second; }
-    holding_pointer_t extract_back() { return extract(std::prev(this->end())).second; }
-    iterator erase(const_iterator pos) { return extract(pos).first; }
-    void pop_front() { extract(this->begin()); }
-    void pop_back() { extract(std::prev(this->end())); }
-
-    static const_iterator to_iterator(const parent_t& p) noexcept {
-        return const_iterator(get_hook(const_cast<parent_t&>(p)));
+    owning_pointer_t extract_front() { return extract(this->begin()).second; }
+    owning_pointer_t extract_back() { return extract(std::prev(this->end())).second; }
+    void pop_front() { node_traits::dispose(extract(this->begin()).second); }
+    void pop_back() { node_traits::dispose(extract(std::prev(this->end())).second); }
+    iterator erase(const_iterator pos) {
+        auto result = extract(pos);
+        node_traits::dispose(std::move(result.second));
+        return result.first;
     }
-    static iterator to_iterator(parent_t& p) noexcept { return iterator(get_hook(p)); }
+
+    static const_iterator to_iterator(const parent_t& obj) noexcept {
+        return const_iterator(get_hook(const_cast<parent_t&>(obj)));
+    }
+    static iterator to_iterator(parent_t& obj) noexcept { return iterator(get_hook(obj)); }
 
  private:
-    template<typename ParentTy, typename HookGetter_ = HookGetter,
-             typename = std::enable_if_t<!std::is_same<HookGetter_, void>::value>>
-    static hook_t* get_hook(ParentTy& parent) {
-        return HookGetter::get_hook(parent);
+    template<typename ParentTy, typename HookGetter_ = HookGetter>
+    static auto get_hook(ParentTy& obj) -> decltype(HookGetter_{}.get_hook(obj)) {
+        return HookGetter{}.get_hook(obj);
     }
     template<typename ParentTy, typename... Dummy>
-    static hook_t* get_hook(ParentTy& parent, Dummy&&...) {
-        return &parent;
-    }
-
-    template<typename HookTraits_ = HookTraits, typename = std::enable_if_t<HookTraits_::is_release_modifying::value>>
-    void release_all() {
-#if _ITERATOR_DEBUG_LEVEL != 0
-        auto* item = this->head_.next;
-        while (item != &this->head_) {
-            auto* next = item->next;
-            node_traits::set_head(item, nullptr);
-            item = next;
-        }
-#endif  // _ITERATOR_DEBUG_LEVEL != 0
-    }
-    template<typename... Dummy>
-    void release_all(Dummy&&...) {
-        auto* item = this->head_.next;
-        while (item != &this->head_) {
-            auto* next = item->next;
-            node_traits::set_head(item, nullptr);
-            hook_traits::release(static_cast<hook_t*>(item));
-            item = next;
-        }
+    static hook_t* get_hook(ParentTy& obj, Dummy&&...) {
+        return &obj;
     }
 };
 
