41ba9e608d6863da6f11faca33a5af0dfdb41cc1
Georgii Buzykin <gbuzykin@yandex.ru>
2024-05-02 23:15:50 +0300

    format: Introduce format parser context

--- src/format.cpp
+++ src/format.cpp
@@ -4,7 +4,7 @@ using namespace uxs;
 
 namespace uxs {
 namespace sfmt {
-template UXS_EXPORT void vformat(format_context, std::string_view);
-template UXS_EXPORT void vformat(wformat_context, std::wstring_view);
+template UXS_EXPORT void vformat(format_context, format_parse_context);
+template UXS_EXPORT void vformat(wformat_context, wformat_parse_context);
 }  // namespace sfmt
 }  // namespace uxs
