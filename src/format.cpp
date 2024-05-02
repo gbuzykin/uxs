b58a77939358476f9efb69ef87e4fa69c4e082ca
Georgii Buzykin <gbuzykin@yandex.ru>
2024-05-02 18:51:06 +0300

    format: Replace meta characters to real characters in format string parser

--- src/format.cpp
+++ src/format.cpp
@@ -4,9 +4,6 @@ using namespace uxs;
 
 namespace uxs {
 namespace sfmt {
-#if __cplusplus < 201703L
-const meta_tbl_t g_meta_tbl;
-#endif  // __cplusplus < 201703L
 template UXS_EXPORT membuffer& vformat(membuffer&, std::string_view, format_args, const std::locale*);
 template UXS_EXPORT wmembuffer& vformat(wmembuffer&, std::wstring_view, wformat_args, const std::locale*);
 }  // namespace sfmt
