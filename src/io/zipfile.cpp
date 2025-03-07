fda0c7354aacc904e5302cde54e7deaead9140bd
Georgii Buzykin <gbuzykin@yandex.ru>
2025-03-07 17:55:48 +0300

    zip: Various improvements

--- src/io/zipfile.cpp
+++ src/io/zipfile.cpp
@@ -11,50 +11,74 @@ using namespace uxs;
 bool zipfile::open(ziparch& arch, const char* fname, iomode mode) {
     if (!arch.zip_) { return false; }
     close();
+    zip_t* zip = static_cast<zip_t*>(arch.zip_);
     if (!!(mode & iomode::out)) {
-        zip_source_t* source = ::zip_source_buffer_create(nullptr, 0, 0, nullptr);
-        if (!source) { return false; }
-        if (::zip_source_begin_write(source) != 0) {
+        mode &= ~iomode::in;
+        zip_source_t* source = ::zip_source_buffer(zip, nullptr, 0, 0);
+        if (!source || ::zip_source_begin_write(source) != 0) {
             ::zip_source_free(source);
             return false;
         }
-        fname_ = fname;
-        zip_arch_ = arch.zip_;
-        zip_fd_ = source;
+        try {
+            zip_fdesc_ = new writing_desc_t{std::string{fname}, zip, source};
+        } catch (...) {
+            ::zip_source_free(source);
+            throw;
+        }
     } else if (!!(mode & iomode::in)) {
-        zip_fd_ = ::zip_fopen(static_cast<zip_t*>(arch.zip_), fname, ZIP_FL_UNCHANGED);
-        if (!zip_fd_) { return false; }
+        zip_fdesc_ = ::zip_fopen(zip, fname, ZIP_FL_ENC_UTF_8 | ZIP_FL_UNCHANGED);
+        if (!zip_fdesc_) { return false; }
+    } else {
+        return false;
+    }
+    mode_ = mode;
+    return true;
+}
+
+bool zipfile::open(ziparch& arch, std::uint64_t index, iomode mode) {
+    if (!arch.zip_) { return false; }
+    close();
+    if (!(mode & iomode::out) && !!(mode & iomode::in)) {
+        zip_t* zip = static_cast<zip_t*>(arch.zip_);
+        zip_fdesc_ = ::zip_fopen_index(zip, static_cast<zip_uint64_t>(index), ZIP_FL_UNCHANGED);
+        if (!zip_fdesc_) { return false; }
+    } else {
+        return false;
     }
     mode_ = mode;
     return true;
 }
 
 void zipfile::close() noexcept {
-    if (!zip_fd_) { return; }
+    if (!zip_fdesc_) { return; }
     if (!!(mode_ & iomode::out)) {
-        zip_source_t* source = static_cast<zip_source_t*>(zip_fd_);
+        const writing_desc_t* wr_desc = static_cast<writing_desc_t*>(zip_fdesc_);
+        zip_t* zip = static_cast<zip_t*>(wr_desc->zip_arch);
+        zip_source_t* source = static_cast<zip_source_t*>(wr_desc->zip_source);
         if (::zip_source_commit_write(source) != 0 ||
-            ::zip_file_add(static_cast<zip_t*>(zip_arch_), fname_.c_str(), source, ZIP_FL_ENC_UTF_8) < 0) {
+            ::zip_file_add(zip, wr_desc->fname.c_str(), source, ZIP_FL_ENC_UTF_8) < 0) {
             ::zip_source_free(source);
         }
+        delete wr_desc;
     } else {
-        ::zip_fclose(static_cast<zip_file_t*>(zip_fd_));
+        ::zip_fclose(static_cast<zip_file_t*>(zip_fdesc_));
     }
-    zip_arch_ = nullptr;
-    zip_fd_ = nullptr;
+    zip_fdesc_ = nullptr;
 }
 
 int zipfile::read(void* data, std::size_t sz, std::size_t& n_read) {
-    if (!zip_fd_ || !(mode_ & iomode::in)) { return -1; }
-    const zip_int64_t result = ::zip_fread(static_cast<zip_file_t*>(zip_fd_), data, sz);
+    if (!zip_fdesc_ || !(mode_ & iomode::in)) { return -1; }
+    const zip_int64_t result = ::zip_fread(static_cast<zip_file_t*>(zip_fdesc_), data, static_cast<zip_uint64_t>(sz));
     if (result < 0) { return -1; }
     n_read = static_cast<std::size_t>(result);
     return 0;
 }
 
 int zipfile::write(const void* data, std::size_t sz, std::size_t& n_written) {
-    if (!zip_fd_ || !(mode_ & iomode::out)) { return -1; }
-    const zip_int64_t result = ::zip_source_write(static_cast<zip_source_t*>(zip_fd_), data, sz);
+    if (!zip_fdesc_ || !(mode_ & iomode::out)) { return -1; }
+    const writing_desc_t* wr_desc = static_cast<writing_desc_t*>(zip_fdesc_);
+    zip_source_t* source = static_cast<zip_source_t*>(wr_desc->zip_source);
+    const zip_int64_t result = ::zip_source_write(source, data, static_cast<zip_uint64_t>(sz));
     if (result < 0) { return -1; }
     n_written = static_cast<std::size_t>(result);
     return 0;
@@ -63,10 +87,11 @@ int zipfile::write(const void* data, std::size_t sz, std::size_t& n_written) {
 #else  // defined(UXS_USE_LIBZIP)
 
 using namespace uxs;
-bool zipfile::open(ziparch& arch, const char* fname, iomode mode) { return false; }
+bool zipfile::open(ziparch& /*arch*/, const char* /*fname*/, iomode /*mode*/) { return false; }
+bool zipfile::open(ziparch& /*arch*/, std::uint64_t /*index*/, iomode /*mode*/) { return false; }
 void zipfile::close() noexcept {}
-int zipfile::read(void* data, std::size_t sz, std::size_t& n_read) { return -1; }
-int zipfile::write(const void* data, std::size_t sz, std::size_t& n_written) { return -1; }
+int zipfile::read(void* /*data*/, std::size_t /*sz*/, std::size_t& /*n_read*/) { return -1; }
+int zipfile::write(const void* /*data*/, std::size_t /*sz*/, std::size_t& /*n_written*/) { return -1; }
 
 #endif  // defined(UXS_USE_LIBZIP)
 
