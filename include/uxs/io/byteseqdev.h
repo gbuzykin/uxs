3d36110ab98ab948d748283ff66717e24a9417b5
Georgii Buzykin <gbuzykin@yandex.ru>
2025-03-10 09:51:25 +0300

    byteseq, byteseqdev: Make as template with abstract allocator parameter

--- include/uxs/io/byteseqdev.h
+++ include/uxs/io/byteseqdev.h
@@ -6,17 +6,18 @@
 
 namespace uxs {
 
-class UXS_EXPORT_ALL_STUFF_FOR_GNUC byteseqdev : public iodevice {
+template<typename Alloc>
+class basic_byteseqdev : public iodevice {
  public:
-    explicit byteseqdev(const byteseq& seq) noexcept
-        : iodevice(iodevcaps::rdonly | iodevcaps::mappable), seq_(&const_cast<byteseq&>(seq)),
+    explicit basic_byteseqdev(const basic_byteseq<Alloc>& seq) noexcept
+        : iodevice(iodevcaps::rdonly | iodevcaps::mappable), seq_(&const_cast<basic_byteseq<Alloc>&>(seq)),
           chunk_(seq_->head_ ? seq_->head_->next : nullptr) {}
-    explicit byteseqdev(byteseq& seq) noexcept
+    explicit basic_byteseqdev(basic_byteseq<Alloc>& seq) noexcept
         : iodevice(iodevcaps::mappable), seq_(&seq), chunk_(seq_->head_ ? seq_->head_->next : nullptr) {}
-    UXS_EXPORT byteseqdev(byteseqdev&& other) noexcept;
-    UXS_EXPORT byteseqdev& operator=(byteseqdev&& other) noexcept;
+    UXS_EXPORT basic_byteseqdev(basic_byteseqdev&& other) noexcept;
+    UXS_EXPORT basic_byteseqdev& operator=(basic_byteseqdev&& other) noexcept;
 
-    const byteseq* get() const noexcept { return seq_; }
+    const basic_byteseq<Alloc>* get() const noexcept { return seq_; }
     UXS_EXPORT void clear() noexcept;
     UXS_EXPORT int read(void* data, std::size_t sz, std::size_t& n_read) override;
     UXS_EXPORT int write(const void* data, std::size_t sz, std::size_t& n_written) override;
@@ -27,10 +28,12 @@ class UXS_EXPORT_ALL_STUFF_FOR_GNUC byteseqdev : public iodevice {
     int flush() override { return 0; }
 
  private:
-    byteseq* seq_;
-    byteseq::chunk_t* chunk_ = nullptr;
+    basic_byteseq<Alloc>* seq_;
+    typename basic_byteseq<Alloc>::chunk_t* chunk_ = nullptr;
     std::size_t pos0_ = 0;
     std::size_t pos_ = 0;
 };
+
+using byteseqdev = basic_byteseqdev<std::allocator<std::uint8_t>>;
 
 }  // namespace uxs
