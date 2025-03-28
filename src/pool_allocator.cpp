bbe7e30bc4569383d0ab343d0ed3e046c7c527e4
Georgii Buzykin <gbuzykin@yandex.ru>
2025-03-28 17:59:26 +0300

    Remove legacy files

--- src/pool_allocator.cpp
+++
@@ -1,8 +0,0 @@
-#include "uxs/impl/pool_allocator_impl.h"
-
-namespace uxs {
-namespace detail {
-UXS_EXPORT pool<std::allocator<void>> g_global_pool;
-template class pool<std::allocator<void>>;
-}  // namespace detail
-}  // namespace uxs
