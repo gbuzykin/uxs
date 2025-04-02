2b4bc1334915e78b5add228d52a85c799691eb4b
Georgii Buzykin <gbuzykin@yandex.ru>
2025-04-02 21:39:57 +0300

    guid: Move to `constexpr` constructors

--- src/guid.cpp
+++ src/guid.cpp
@@ -16,10 +16,10 @@ std::mt19937 g_generator(g_seed);
 /*static*/ guid guid::generate() {
     guid id;
     std::uniform_int_distribution<std::uint32_t> distribution(0, std::numeric_limits<std::uint32_t>::max());
-    for (std::uint32_t& l : id.data32_) { l = distribution(g_generator); }
+    for (std::uint32_t& l : id.data32) { l = distribution(g_generator); }
     // set version: must be 0b0100xxxx
-    id.data8_[7] = (id.data8_[7] & 0x4F) | 0x40;
+    id.data8[7] = (id.data8[7] & 0x4F) | 0x40;
     // set variant: must be 0b10xxxxxx
-    id.data8_[8] = (id.data8_[8] & 0xBF) | 0x80;
+    id.data8[8] = (id.data8[8] & 0xBF) | 0x80;
     return id;
 }
