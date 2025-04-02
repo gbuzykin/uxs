39d4405901b46dabfc29337bc40ad8b1a56bc4a5
Georgii Buzykin <gbuzykin@yandex.ru>
2025-04-02 21:39:57 +0300

    guid: Move to `constexpr` constructors

--- include/uxs/guid.h
+++ include/uxs/guid.h
@@ -6,45 +6,45 @@
 
 namespace uxs {
 
-class guid {
- public:
-    guid() noexcept { data64_[0] = data64_[1] = 0; }
-    explicit guid(const std::uint32_t* id) noexcept { set_data(id); }
-    guid(std::uint32_t l, std::uint16_t w1, std::uint16_t w2, std::uint8_t b1, std::uint8_t b2, std::uint8_t b3,
-         std::uint8_t b4, std::uint8_t b5, std::uint8_t b6, std::uint8_t b7, std::uint8_t b8) noexcept {
-        data32_[0] = l, data16_[2] = w1, data16_[3] = w2;
-        data8_[8] = b1, data8_[9] = b2, data8_[10] = b3, data8_[11] = b4;
-        data8_[12] = b5, data8_[13] = b6, data8_[14] = b7, data8_[15] = b8;
+struct guid {
+    struct layout_t {
+        UXS_CONSTEXPR layout_t(std::uint32_t l, std::uint16_t w1, std::uint16_t w2, std::uint8_t b1, std::uint8_t b2,
+                               std::uint8_t b3, std::uint8_t b4, std::uint8_t b5, std::uint8_t b6, std::uint8_t b7,
+                               std::uint8_t b8) noexcept
+            : l{l}, w{w1, w2}, b{b1, b2, b3, b4, b5, b6, b7, b8} {}
+        std::uint32_t l;
+        std::array<std::uint16_t, 2> w;
+        std::array<std::uint8_t, 8> b;
+    };
+
+    union {
+        layout_t layout;
+        std::array<std::uint8_t, 16> data8;
+        std::array<std::uint16_t, 8> data16;
+        std::array<std::uint32_t, 4> data32;
+        std::array<std::uint64_t, 2> data64;
+    };
+
+    UXS_CONSTEXPR guid() noexcept : data64{0, 0} {}
+    UXS_CONSTEXPR guid(std::array<std::uint32_t, 4> l) noexcept : data32{l} {}
+    UXS_CONSTEXPR guid(std::uint32_t l, std::uint16_t w1, std::uint16_t w2, std::uint8_t b1, std::uint8_t b2,
+                       std::uint8_t b3, std::uint8_t b4, std::uint8_t b5, std::uint8_t b6, std::uint8_t b7,
+                       std::uint8_t b8) noexcept
+        : layout{l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8} {}
+
+    bool valid() const noexcept { return data64[0] || data64[1]; }
+    guid make_xor(std::uint32_t a) const noexcept {
+        guid id;
+        id.data32[0] = data32[0] ^ a, id.data32[1] = data32[1] ^ a;
+        id.data32[2] = data32[2] ^ a, id.data32[3] = data32[3] ^ a;
+        return id;
     }
 
-    bool valid() const noexcept { return data64_[0] || data64_[1]; }
-    guid make_xor(std::uint32_t a) const noexcept;
-
-    std::uint8_t data8(unsigned i) const noexcept { return data8_[i]; }
-    std::uint16_t data16(unsigned i) const noexcept { return data16_[i]; }
-    std::uint32_t data32(unsigned i) const noexcept { return data32_[i]; }
-    std::uint64_t data64(unsigned i) const noexcept { return data64_[i]; }
-
-    std::uint8_t& data8(unsigned i) noexcept { return data8_[i]; }
-    std::uint16_t& data16(unsigned i) noexcept { return data16_[i]; }
-    std::uint32_t& data32(unsigned i) noexcept { return data32_[i]; }
-    std::uint64_t& data64(unsigned i) noexcept { return data64_[i]; }
-
-    void get_data(std::uint32_t* id) const noexcept {
-        id[0] = data32_[0], id[1] = data32_[1], id[2] = data32_[2], id[3] = data32_[3];
-    }
-
-    void set_data(const std::uint32_t* id) noexcept {
-        data32_[0] = id[0], data32_[1] = id[1], data32_[2] = id[2], data32_[3] = id[3];
-    }
-
-    bool operator==(const guid& id) const noexcept {
-        return data64_[0] == id.data64_[0] && data64_[1] == id.data64_[1];
-    }
+    bool operator==(const guid& id) const noexcept { return data64[0] == id.data64[0] && data64[1] == id.data64[1]; }
     bool operator!=(const guid& id) const noexcept { return !(*this == id); }
 
     bool operator<(const guid& id) const noexcept {
-        return data64_[0] < id.data64_[0] || (data64_[0] == id.data64_[0] && data64_[1] < id.data64_[1]);
+        return data64[0] < id.data64[0] || (data64[0] == id.data64[0] && data64[1] < id.data64[1]);
     }
     bool operator<=(const guid& id) const noexcept { return !(id < *this); }
     bool operator>(const guid& id) const noexcept { return id < *this; }
@@ -70,28 +70,13 @@ class guid {
     static guid from_per_byte_wstring(std::wstring_view s) noexcept { return from_per_byte_basic_string(s); }
 
     UXS_EXPORT static guid generate();
-
- private:
-    union {
-        std::array<std::uint8_t, 16> data8_;
-        std::array<std::uint16_t, 8> data16_;
-        std::array<std::uint32_t, 4> data32_;
-        std::array<std::uint64_t, 2> data64_;
-    };
 };
-
-inline guid guid::make_xor(std::uint32_t a) const noexcept {
-    guid id;
-    id.data32_[0] = data32_[0] ^ a, id.data32_[1] = data32_[1] ^ a;
-    id.data32_[2] = data32_[2] ^ a, id.data32_[3] = data32_[3] ^ a;
-    return id;
-}
 
 template<typename CharT, typename Traits, typename Alloc>
 void guid::to_per_byte_basic_string(std::basic_string<CharT, Traits, Alloc>& s) const {
     s.resize(32);
     auto* p = &s[0];
-    for (const std::uint8_t b : data8_) { to_hex(b, p, 2, true), p += 2; }
+    for (const std::uint8_t b : data8) { to_hex(b, p, 2, true), p += 2; }
 }
 
 template<typename CharT, typename Traits>
@@ -99,7 +84,7 @@ template<typename CharT, typename Traits>
     guid id;
     if (s.size() < 32) { return id; }
     const auto* p = s.data();
-    for (std::uint8_t& b : id.data8_) { b = from_hex(p, 2), p += 2; }
+    for (std::uint8_t& b : id.data8) { b = from_hex(p, 2), p += 2; }
     return id;
 }
 
@@ -109,13 +94,13 @@ struct from_string_impl<guid, CharT> {
         const std::size_t len = 38;
         if (static_cast<std::size_t>(last - first) < len) { return 0; }
         const auto* p = first;
-        val.data32(0) = from_hex(p + 1, 8);
-        val.data16(2) = from_hex(p + 10, 4);
-        val.data16(3) = from_hex(p + 15, 4);
-        val.data8(8) = from_hex(p + 20, 2);
-        val.data8(9) = from_hex(p + 22, 2);
+        val.layout.l = from_hex(p + 1, 8);
+        val.layout.w[0] = from_hex(p + 10, 4);
+        val.layout.w[1] = from_hex(p + 15, 4);
+        val.layout.b[0] = from_hex(p + 20, 2);
+        val.layout.b[1] = from_hex(p + 22, 2);
         p += 25;
-        for (unsigned i = 10; i < 16; ++i, p += 2) { val.data8(i) = from_hex(p, 2); }
+        for (unsigned i = 2; i < 8; ++i, p += 2) { val.layout.b[i] = from_hex(p, 2); }
         return first + len;
     }
 };
@@ -129,13 +114,13 @@ struct to_string_impl<guid, CharT> {
         std::array<typename StrTy::value_type, len> buf;
         auto* p = buf.data();
         p[0] = '{', p[9] = '-', p[14] = '-', p[19] = '-', p[24] = '-', p[37] = '}';
-        to_hex(val.data32(0), p + 1, 8, upper);
-        to_hex(val.data16(2), p + 10, 4, upper);
-        to_hex(val.data16(3), p + 15, 4, upper);
-        to_hex(val.data8(8), p + 20, 2, upper);
-        to_hex(val.data8(9), p + 22, 2, upper);
+        to_hex(val.layout.l, p + 1, 8, upper);
+        to_hex(val.layout.w[0], p + 10, 4, upper);
+        to_hex(val.layout.w[1], p + 15, 4, upper);
+        to_hex(val.layout.b[0], p + 20, 2, upper);
+        to_hex(val.layout.b[1], p + 22, 2, upper);
         p += 25;
-        for (unsigned i = 10; i < 16; ++i, p += 2) { to_hex(val.data8(i), p, 2, upper); }
+        for (unsigned i = 2; i < 8; ++i, p += 2) { to_hex(val.layout.b[i], p, 2, upper); }
         const auto fn = [&buf](StrTy& s) { s.append(buf.data(), buf.size()); };
         fmt.width > len ? append_adjusted(s, fn, len, fmt) : fn(s);
     }
@@ -175,7 +160,7 @@ namespace std {
 template<>
 struct hash<uxs::guid> {
     std::size_t operator()(uxs::guid id) const {
-        return hash<std::uint64_t>{}(id.data64(0)) ^ (hash<std::uint64_t>{}(id.data64(1)) << 1);
+        return hash<std::uint64_t>{}(id.data64[0]) ^ (hash<std::uint64_t>{}(id.data64[1]) << 1);
     }
 };
 }  // namespace std
