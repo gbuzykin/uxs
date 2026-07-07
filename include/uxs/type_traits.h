2d87a13ad21c692a879cb7036666ca3a89b9ca78
Georgii Buzykin <gbuzykin@yandex.ru>
2026-07-07 17:19:31 +0300

    Introduce `type_traits.h` header

--- include/uxs/type_traits.h
+++ include/uxs/type_traits.h
@@ -31,6 +31,20 @@ template<typename Ty1, typename Ty2, typename... Rest>
 struct sum<Ty1, Ty2, Rest...>
     : std::integral_constant<typename Ty1::value_type, Ty1::value + sum<Ty2, Rest...>::value> {};
 
+template<typename...>
+struct minimum;
+template<typename Ty>
+struct minimum<Ty> : Ty {};
+template<typename Ty1, typename Ty2, typename... Rest>
+struct minimum<Ty1, Ty2, Rest...> : minimum<std::conditional_t<(Ty1::value < Ty2::value), Ty1, Ty2>, Rest...> {};
+
+template<typename...>
+struct maximum;
+template<typename Ty>
+struct maximum<Ty> : Ty {};
+template<typename Ty1, typename Ty2, typename... Rest>
+struct maximum<Ty1, Ty2, Rest...> : maximum<std::conditional_t<(Ty1::value < Ty2::value), Ty2, Ty1>, Rest...> {};
+
 template<std::size_t I, typename...>
 struct type_pack_element;
 template<std::size_t I, typename Ty, typename... Rest>
@@ -40,42 +54,18 @@ struct type_pack_element<0, Ty, Rest...> {
     using type = Ty;
 };
 
-#if __cplusplus < 201402L
-template<typename...>
-struct minimum;
+template<typename... Ts>
+struct size_of : maximum<size_of<Ts>...> {};
 template<typename Ty>
-struct minimum<Ty> : Ty {};
-template<typename Ty1, typename Ty2, typename... Rest>
-struct minimum<Ty1, Ty2, Rest...> : minimum<std::conditional_t<(Ty1::value < Ty2::value), Ty1, Ty2>, Rest...> {};
-template<typename...>
-struct maximum;
-template<typename Ty>
-struct maximum<Ty> : Ty {};
-template<typename Ty1, typename Ty2, typename... Rest>
-struct maximum<Ty1, Ty2, Rest...> : maximum<std::conditional_t<(Ty1::value < Ty2::value), Ty2, Ty1>, Rest...> {};
-#else   // __cplusplus < 201402L
-namespace detail {
-template<typename Ty>
-constexpr Ty min_impl(std::initializer_list<Ty> l) {
-    auto it0 = l.begin();
-    for (auto it = l.begin() + 1; it != l.end(); ++it) {
-        if (*it < *it0) { it0 = it; }
-    }
-    return *it0;
-}
-template<typename Ty>
-constexpr Ty max_impl(std::initializer_list<Ty> l) {
-    auto it0 = l.begin();
-    for (auto it = l.begin() + 1; it != l.end(); ++it) {
-        if (*it > *it0) { it0 = it; }
-    }
-    return *it0;
-}
-}  // namespace detail
-template<typename Ty1, typename... Ts>
-struct minimum : std::integral_constant<typename Ty1::value_type, detail::min_impl({Ty1::value, Ts::value...})> {};
-template<typename Ty1, typename... Ts>
-struct maximum : std::integral_constant<typename Ty1::value_type, detail::max_impl({Ty1::value, Ts::value...})> {};
-#endif  // __cplusplus < 201402L
+struct size_of<Ty> : std::integral_constant<std::size_t, sizeof(Ty)> {};
+template<typename... Ts>
+struct alignment_of : maximum<std::alignment_of<Ts>...> {};
+
+template<std::size_t Alignment>
+struct align_up {
+    template<std::size_t Size>
+    struct type : std::integral_constant<std::size_t, (Size + Alignment - 1) & ~(Alignment - 1)> {};
+    static std::size_t value(std::size_t sz) { return (sz + Alignment - 1) & ~(Alignment - 1); }
+};
 
 }  // namespace est
